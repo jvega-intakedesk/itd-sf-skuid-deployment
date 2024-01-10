a2c232e7c87819a8d97f707182db7574bb9a0237
Cristiano Diniz da Silva <csilva@intakedesk.com>
2024-01-10 15:59:03 -0500

    IS-707: Adjusting the command to properly pass the username
    Fixing an error on github action where the username was being
    validaded as empty and adding a flag.

--- local-sync.sh
+++ local-sync.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-sf skuid page pull --nomodule --targetusername=csilva@intakedesk.com
+flags=()
+username_alias='csilva@intakedesk.com'
+
+if [ ! -z "$username_alias" ]; then
+syncFlags+=( --targetusername=$username_alias )
+fi
+
+sf skuid page pull --nomodule "${syncFlags[@]}"
 
 modules=Intake_Modules,Old_Pages,Test-DEV,Verification_Modules
 for i in $(echo $modules | sed "s/,/ /g")
 do
     module=$(echo $i | sed 's/_/ /g')
-    echo "sf skuid page pull --module=\"${module}\" --targetusername=csilva@intakedesk.com"
-    sf skuid page pull --module="${module}" --targetusername=csilva@intakedesk.com
+    echo "sf skuid page pull --module=\"${module}\" \"${syncFlags[@]}\""
+    sf skuid page pull --module="${module}" "${syncFlags[@]}"
     sleep 5
 done
 
 for i in $(echo $modules | sed "s/,/ /g")
 do
     module=$(echo $i | sed 's/_/ /g')
-    echo "sf skuid page push --module=\"${module}\" --targetusername=csilva@intakedesk.com"
-    sf skuid page push --module="${module}" --targetusername=csilva@intakedesk.com
+    echo "sf skuid page push --module=\"${module}\" \"${syncFlags[@]}\""
+    sf skuid page push --module="${module}" "${syncFlags[@]}"
     sleep 5
 done
